5b1a6875e86edf00f6020dbf47d8bfdd79296f71
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-11-04 19:49:28 +0500

    Update __manifest__.py

--- gts_contacts_whatsapp/__manifest__.py
+++ gts_contacts_whatsapp/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    'name': 'Whatsapp Messaging for Contacts',
-    'description': "Adds whatsapp messaging functionality to the 'Contacts' app.",
-    'author': 'Muhammad Ahmad',
+    'name': 'Import Whatsapp Contacts in Odoo',
+    'description': 'static/description/index.html',
+    'author': 'WLink',
     'license': 'LGPL-3',
-    'version': '18.0.1.0',
-    'depends': ['gts_whatsapp', 'contacts', 'sale', 'mail'],
+    'version': '17.0.1.0',
+    'depends': ['gts_whatsapp', 'contacts', 'sale'],
+    'images': ['static/description/banner.png']
     'data': [
         'security/ir.model.access.csv',
 
