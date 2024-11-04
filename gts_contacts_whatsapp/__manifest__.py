45b416b6cc3777e37c43138360f5562772a1570a
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-11-04 16:15:26 +0500

    Update __manifest__.py

--- gts_contacts_whatsapp/__manifest__.py
+++ gts_contacts_whatsapp/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    'name': 'Whatsapp Messaging for Contacts',
-    'description': "Adds whatsapp messaging functionality to the 'Contacts' app.",
-    'author': 'Muhammad Ahmad',
+    'name': 'Qr Whatsapp in Contacts app',
+    'description': 'static/description/index.html',
+    'author': 'WLink',
     'license': 'LGPL-3',
     'version': '17.0.1.0',
-    'depends': ['gts_whatsapp', 'contacts', 'sale', 'mail'],
+    'website':'https://wlink.geektechsol.com',
+    'depends': ['gts_whatsapp', 'contacts', 'sale'],
+    'images': ['static/description/banner.png',],
     'data': [
         'security/ir.model.access.csv',
 
