cd3df84d73cb28ed4d666df3b0938e07c391b4d3
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-11-04 16:16:55 +0500

    Update __manifest__.py

--- gts_whatsapp_pos/__manifest__.py
+++ gts_whatsapp_pos/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    'name': 'Whatsapp Messaging for POS',
-    'description': 'static/description/index.html',
+    'name': 'Qr Whatsapp POS Receipts',
+    'description':'static/description/index.html',
     'author': 'WLink',
     'license': 'LGPL-3',
     'version': '17.0.1.0',
     'depends': ['gts_whatsapp', 'gts_contacts_whatsapp', 'point_of_sale', 'account'],
+    'images': ['static/description/banner_pos.png'],
+    'website':'https://wlink.geektechsol.com',
     'data': [
         'security/ir.model.access.csv',
         'wizard/select_connection.xml'
